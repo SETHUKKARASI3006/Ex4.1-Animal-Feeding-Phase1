831c9426c58a327be34877fd860374dc411ab3f9
SETHUKKARASI3006 <userarasi@gmail.com>
2024-04-24 21:24:29 +0530

    added

--- Assets/Detect.cs
+++ Assets/Detect.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Detect : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -16,5 +15,9 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
 }
